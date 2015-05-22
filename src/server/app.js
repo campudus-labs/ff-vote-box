af6dfc56ea1b38fc054a819d980ffea9bafa134b
Alexander Vetter <vettera@gmail.com>
2015-05-22 17:23:00 +0200

    ability to login as bob:secret

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser';
+import passport from 'passport';
+import authRouter from './auth';
 import topicRouter from './topicRouter';
 
 let app = express();
-let topicIdCounter = 0;
-let ideaIdCounter = 0;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.use(cookieParser("secret"));
+app.use(session({secret : 'keyboard cat', saveUninitialized : true, resave : true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(authRouter);
 app.use(topicRouter);
 
 app.listen(8181);
--- src/server/auth.js
+++ src/server/auth.js
@@ -1,7 +1,42 @@
 import express from 'express';
 import passport from 'passport';
+import {Strategy as LocalStrategy} from 'passport-local';
 
 let authRouter = express.Router();
+
+var users = [
+  {id : 1, username : 'bob', password : 'secret', email : 'bob@example.com'},
+  {id : 2, username : 'joe', password : 'birthday', email : 'joe@example.com'}
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 authRouter.get('/logout', function (req, res) {
   req.logout();
@@ -16,19 +51,24 @@ authRouter.post('/login', passport.authenticate('local'), function (req, res) {
   res.end('Logged in');
 });
 
-class Authenticator {
-  constructor() {
-    this.value = 0;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    process.nextTick(function () {
+
+      findByUsername(username, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, {message : 'Unknown user ' + username});
+        }
+        if (user.password != password) {
+          return done(null, false, {message : 'Invalid password'});
+        }
+        return done(null, user);
+      })
+    });
   }
+));
 
-  check(username, password, done) {
-    return done(null, {"username" : username});
-  }
-}
-
-export default {
-  ROUTER : authRouter,
-  AUTHENTICATOR : new Authenticator()
-};
-
-
+export default authRouter;
