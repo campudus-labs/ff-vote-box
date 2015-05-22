b6e661758f2eee436e069758cdfd5805ca8864cb
Joern Bernhardt <jb@campudus.com>
2015-05-22 16:26:47 +0200

    Add a few routes
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/server/app.js
+++ src/server/app.js
@@ -1,23 +1,45 @@
 import express from 'express';
-
-import Calculator from './Calculator';
+import bodyParser from 'body-parser';
 
 let app = express();
-let calc = new Calculator();
+let topicIdCounter = 0;
+let ideaIdCounter = 0;
 
-app.get('/votes', function (req, res) {
-  res.send({votes : []});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({	extended: true }));
+
+app.get('/topics', function (req, res) {
+  res.send({topics : []});
 });
 
-app.get('/hello', function (req, res) {
-  let hellos = ['hey', 'hi', 'hello', 'good day', 'welcome'];
-  throw new Error('ups');
-  res.send(hellos[Math.floor(Math.random() * hellos.length)]);
+app.post('/topics', function (req, res) {
+  var topic = {
+    id : (++topicIdCounter),
+    title : req.body.title,
+    description : req.body.description
+  };
+
+  res.send('got some topic:' + JSON.stringify(topic));
 });
 
-app.get('/calc/add/:x', function (req, res) {
-  let x = req.params.x;
-  res.send('current:' + calc.add(x));
+app.post('/topics/:topicId/ideas', function (req, res) {
+  var idea = {
+    id : (++ideaIdCounter),
+    title : req.body.title,
+    description : req.body.description,
+    votes : req.body.votes
+  };
+
+  res.send('got some idea:' + JSON.stringify(idea));
+});
+
+app.post('/login', function (req, res) {
+  var credentials = {
+    username : req.body.username,
+    password : req.body.password
+  };
+
+  res.send('got some login:' + JSON.stringify(credentials));
 });
 
 app.listen(8181);
