0f6c45b48faba25f1dac0a63b8272521a9ed42b5
Joern Bernhardt <jb@campudus.com>
2015-05-22 12:11:14 +0200

    Rename main to frontend, add basic server
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -20,17 +20,19 @@ gulp.task('assets', assetCopy);
 gulp.task('scripts', scriptCompile);
 gulp.task('clean', clean);
 
-gulp.task('reloader', ['build'], reload);
-gulp.task('dev', ['build'], server);
+gulp.task('frontendReload', ['build'], reload);
+gulp.task('dev', ['build'], frontendServer);
 gulp.task('test', ['build'], test);
 gulp.task('testWatch', ['build'], testWatch);
 
-gulp.task('build', ['sass', 'assets', 'scripts']);
+gulp.task('build:frontend', ['sass', 'assets', 'scripts']);
+gulp.task('build:server', serverScripts);
+gulp.task('build', ['build:frontend', 'build:server']);
 gulp.task('default', ['build']);
 
 
 function sassCompile() {
-  return gulp.src('src/main/scss/main.scss')
+  return gulp.src('src/frontend/scss/main.scss')
     .pipe(plumber({
       errorHandler : function (error) {
         console.log(error.message);
@@ -39,31 +41,39 @@ function sassCompile() {
     }))
     .pipe(compass({
       project : Path.join(__dirname),
-      css : 'dist/css',
-      sass : 'src/main/scss',
-      image : 'src/main/img'
+      css : 'dist/frontend/css',
+      sass : 'src/frontend/scss',
+      image : 'src/frontend/img'
     }))
     .pipe(minifyCss())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/frontend/css'));
 }
 
 function scriptCompile() {
+  return compiledEs6('./src/frontend/js/app.js', 'app.js', './dist/frontend/js');
+}
+
+function serverScripts() {
+  return compiledEs6('./src/server/js/app.js', 'app.js', './dist/server/');
+}
+
+function compiledEs6(inFile, outFile, outDir) {
   return browserify()
     .transform(babelify)
     .transform(reactify)
-    .add('./src/main/js/app.js')
+    .add(inFile)
     .bundle()
     .on('error', function (err) {
       console.log('error', err);
       this.emit('end');
     })
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(source(outFile))
+    .pipe(gulp.dest(outDir));
 }
 
 function assetCopy() {
-  return gulp.src(['src/main/**', '!src/main/js/**', '!src/main/scss', '!src/main/scss/**'])
-    .pipe(gulp.dest('dist/'));
+  return gulp.src(['src/frontend/**', '!src/frontend/js/**', '!src/frontend/scss', '!src/frontend/scss/**'])
+    .pipe(gulp.dest('dist/frontend/'));
 }
 
 function test(done) {
@@ -86,18 +96,18 @@ function testWatch(done) {
   }, done);
 }
 
-function server() {
+function frontendServer() {
   var proxyOptions = url.parse('http://localhost:8181/');
   proxyOptions.route = '/api';
 
   browserSync({
     server : {
-      baseDir : 'dist',
+      baseDir : 'dist/frontend',
       middleware : [proxy(proxyOptions)]
     }
   });
 
-  gulp.watch(['src/main/**', 'src/main/js/**', 'src/main/scss/**/*.scss'], {}, ['reloader']);
+  gulp.watch(['src/frontend/**', 'src/frontend/js/**', 'src/frontend/scss/**/*.scss'], {}, ['frontendReload']);
 
   //gulp.src('src/test/**/*Spec.js').pipe(karma({
   //  configFile : 'karma.conf.js',
